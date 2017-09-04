f0670c3ddc47971621803871b9e69c7409784113
AnYang <496812133@qq.com>
2017-09-04 16:16:50 +0800

    init

---
+++ src/observer/Context.java
@@ -0,0 +1,33 @@
+package observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kaifashi on 2017/9/4.
+ */
+public class Context {
+    List<Observer> observerList = null;
+
+
+    public void register(Observer observer) {
+        if (observerList == null) {
+            observerList = new ArrayList<>();
+        }
+        observerList.add(observer);
+    }
+
+    public void unregister(Observer observer){
+        if(observerList!=null){
+            this.observerList.remove(observer);
+        }
+    }
+
+    public void invoke() {
+        if (observerList != null) {
+            for (Observer observer : observerList) {
+                observer.update();
+            }
+        }
+    }
+}
---
+++ src/observer/Observer.java
@@ -0,0 +1,10 @@
+package observer;
+
+/**
+ * Created by kaifashi on 2017/9/4.
+ */
+public class Observer {
+    public void update(){
+
+    }
+}
